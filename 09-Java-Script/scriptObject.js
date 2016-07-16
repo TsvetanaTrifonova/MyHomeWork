d6033b0cee127087d7f4dc5be53efb3b28f46aca
tsvetanatrifonova <cve_ti@yahoo.com>
2016-07-16 09:51:49 +0300

    Task 12 and 13

---
+++ 09-Java-Script/scriptObject.js
@@ -0,0 +1,27 @@
+//task 1
+function ObjectStudent(){
+	var student = {
+		name: 'Ivan',
+		age: '20',
+		experience:'1',
+		mark: '6',
+		learn: function() { return 'learning computer science' },
+		enjoy: function() { return 'enjoing' }
+	}
+
+  console.log('	I.');
+	for (var prop in student){
+	       console.log(prop + ": " + student[prop]);
+	}
+
+	console.log('');
+	console.log('	II.');
+	console.log(student.name);
+	console.log(student.age);
+	console.log(student.experience);
+	console.log(student.mark);
+	console.log(student.learn());
+	console.log(student.enjoy());
+}
+
+bntStudent.addEventListener('click', ObjectStudent);
